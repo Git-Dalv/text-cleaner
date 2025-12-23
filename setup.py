86d38ab365ab2259f027c0e326d2125068965e92
Git-Dalv <vladm1999il@gmail.com>
2025-12-23 15:02:31 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="text-cleaner",
+    version="1.0.0",
+    description="Universal Text Cleaner for web scraping",
+    author="Git-Dalv",
+    author_email="your_email@example.com",
+    url="https://github.com/Git-Dalv/text-cleaner",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
